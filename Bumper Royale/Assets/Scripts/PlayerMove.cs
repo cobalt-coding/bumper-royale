28fe3f4379dcb8cfd8b457cdc4e20ab9bf2edd92
Junha Lee <junhalee02@gmail.com>
2017-09-24 12:23:00 -0700

    Movement

--- Bumper Royale/Assets/Scripts/PlayerMove.cs
+++ Bumper Royale/Assets/Scripts/PlayerMove.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float forwardSpeed = 50f;
+    public float sideSpeed = 100f;
+
+    private void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+
+        //Rigidbody rb = this.GetComponent<Rigidbody>();
+        //rb.AddForce(0, 0, yMove * 1000 * Time.deltaTime);
+        transform.Translate(0, 0, yMove * forwardSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, xMove*sideSpeed*Time.deltaTime, 0));
+    }
+
 }
