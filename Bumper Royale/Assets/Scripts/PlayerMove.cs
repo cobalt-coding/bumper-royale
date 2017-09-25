2f857421bee20001ebd123c781ab4d3ebd168510
Junha Lee <junhalee02@gmail.com>
2017-09-24 21:51:01 -0700

    Removed two lines of code whoa

--- Bumper Royale/Assets/Scripts/PlayerMove.cs
+++ Bumper Royale/Assets/Scripts/PlayerMove.cs
@@ -9,9 +9,7 @@ public class PlayerMove : MonoBehaviour {
     {
         float xMove = Input.GetAxis("Horizontal");
         float yMove = Input.GetAxis("Vertical");
-
-        //Rigidbody rb = this.GetComponent<Rigidbody>();
-        //rb.AddForce(0, 0, yMove * 1000 * Time.deltaTime);
+        
         transform.Translate(0, 0, yMove * forwardSpeed * Time.deltaTime);
         transform.Rotate(new Vector3(0, xMove*sideSpeed*Time.deltaTime, 0));
     }
