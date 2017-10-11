6071b51c75a94c4b0e996f5ec5ee2e66aee71569
Junha Lee <junhalee02@gmail.com>
2017-10-10 21:12:03 -0700

    Basic Camera Movement

--- Bumper Royale/Assets/Scripts/CameraMove.cs
+++ Bumper Royale/Assets/Scripts/CameraMove.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public Transform player;
+    public float offsetY = 10;
+    public float offsetZ = -10;
+
+	void LateUpdate () {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y+offsetY, player.transform.position.z-offsetZ);
+        //this.transform.RotateAround(Vector3.right, player.position, 100 * Time.deltaTime);
 	}
 }
