2919c1307674716f35c5ff6b013942de19499050
Junha Lee <junhalee02@gmail.com>
2017-10-29 16:18:32 -0700

    Testing new moving system in new scene

---
+++ Bumper Royale/Assets/Scripts/WheelMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMove : MonoBehaviour {
+
+    public float speed = 500f;
+
+	void Update () {
+        float move = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0, move * speed * Time.deltaTime, 0));
+    }
+}
